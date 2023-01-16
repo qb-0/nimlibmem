c81abf6c3eedceef1790a602f505c4aac079ba5e
qb <saschap@mail.de>
2023-01-16 17:10:48 +0100

    .

--- test/test.nim
+++ test/test.nim
@@ -62,8 +62,3 @@ if isMainModule:
   # LM_LoadModule / LM_UnloadModule
   discard LM_LoadModule((getCurrentDir() & "/libtestlib.so").cstring, modbuf.addr)
   echo "Unload: ", LM_UnloadModule(modbuf.addr) == LM_TRUE
-
-  # LM_UnloadModule / LM_LoadModuleEx
-  #var currentProc: lm_process_t
-  #discard LM_GetProcess(currentproc.addr)
-  #discard LM_LoadModuleEx(currentProc.addr, (getCurrentDir() & "/libtestlib.so").cstring, modbuf.addr)
